eae6005177f0c9f3301018982cd5e9a6042cd760
Maryna Cherniavska <maryna.cherniavska@zalando.de>
2023-05-31 20:49:27 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("io.klogging:klogging-jvm:0.4.13")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+import io.klogging.Level
+import io.klogging.NoCoLogger
+import io.klogging.config.DEFAULT_CONSOLE
+import io.klogging.config.loggingConfiguration
+import io.klogging.java.LoggerFactory
+import java.time.Instant
+
+private val logger: NoCoLogger = LoggerFactory.getLogger("Main")
+fun main(args: Array<String>) {
+    loggingConfiguration {
+        DEFAULT_CONSOLE()
+        minDirectLogLevel(Level.INFO)
+    }
+    val start = Instant.now()
+    logger.info("Hello Parallel World!")
+    logger.info("Processors: ${Runtime.getRuntime().availableProcessors()}")
+    val tasks = (1..10).map { LongRunningTask(it) }
+        .map { Thread { it.run() } }
+    logger.info("Starting all tasks")
+    tasks.forEach {
+        it.start()
+    }
+    tasks.forEach {
+        it.join()
+    }
+    val duration = Instant.now().toEpochMilli() - start.toEpochMilli()
+    logger.info("Finished all tasks in $duration ms")
+}
+
+// class that runs a long-running task
+class LongRunningTask(private val id: Int) {
+    private val logger: NoCoLogger = LoggerFactory.getLogger("task-${id}")
+    fun run() {
+        logger.info("Starting long-running task $id")
+        Thread.sleep(1000)
+        logger.info("Finished long-running task $id")
+    }
+}
+
